bc3f92615375e97011299df724c90df7786bfa13
Sahan Dissanayake <sahan@roamltd.com>
2018-02-09 14:39:53 +1300

    refactor(naming): define const rather than using an already defined const

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,6 @@ const rewireEslint = require('react-app-rewire-eslint')
 
 /* eslint no-unused-vars: 0 */
 module.exports = function override(config, env) {
-  config = rewireEslint(config, env)
-  return config
+  const configWithESLint = rewireEslint(config, env)
+  return configWithESLint
 }
