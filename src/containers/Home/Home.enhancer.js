0e250f4da7cbf7a5c75ee3e4fa7fcf12bb059c4b
Franco Sioquim <francosioquim@gmail.com>
2018-10-02 16:39:15 +1300

    ci: fix config and test

--- src/containers/Home/Home.enhancer.js
+++ src/containers/Home/Home.enhancer.js
@@ -1,28 +1,6 @@
-import * as routeModule from 'modules/route'
-
 import { compose, setDisplayName } from 'recompose'
-
-import { UserIsAuthenticated } from 'utils/authentication'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-
 export default compose(
-    setDisplayName('Home'),
-    UserIsAuthenticated,
-    connect(
-        // mapStateToProps
-        (state) => {
-            return {}
-        },
-        // mapDispatchToProps
-        (dispatch) => {
-            const actions = {
-                ...routeModule,
-            }
-
-            return {
-                actions: bindActionCreators(actions, dispatch),
-            }
-        }
-    )
+    setDisplayName('Home')
+    // uncomment below to require authentication
+    // UserIsAuthenticated,
 )
